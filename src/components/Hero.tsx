5bc2d939df7f7fc9fcf08a699348dee924629972
realsudarshan <sudarshandhakal40@gmail.com>
2024-10-01 17:56:29 +0545

    hero section fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ return(
     <span className="text-4xl sm:text-6xl md:text-7xl block break-words mx-auto ">Discover the Power of SaaS</span>
     <span className="text-4xl sm:text-6xl md:text-8xl block break-words mxa-auto">Simplicity.</span>
     </div>
-<p className="px-28 mb-9 ">Welcome to SaaSStellar, your gateway to streamlined operations, innovation, enhanced productivity, and limitless growth opportunities.
+<p className="px-5 md:px-28 mb-9 ">Welcome to SaaSStellar, your gateway to streamlined operations, innovation, enhanced productivity, and limitless growth opportunities.
 Discover a brighter future with us.</p>
 <div className="flex justify-center items-center">
 <ProfileForm />
